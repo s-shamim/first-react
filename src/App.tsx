ae4838c620770cd55251618c52b1a5ca70171b3f
Shamim Sheikh <Shamim.Sheikh@rib-software.com>
2025-03-04 15:41:43 +0530

    List group changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import Message from "./Message.tsx";
-function App(){
-  return <div><Message></Message></div>
+import ListGroup from "./components/ListGroup.tsx";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokio", "London", "Paris"];
+
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  }
+
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectedItem} /></div>
 }
 
 export default App
